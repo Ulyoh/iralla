2bec6fd0c880e645f35c71d6a0174bdbc9257766
ulyssey <yohann.m@gmail.com>
2011-11-18 18:54:30 -0500

    class Vector validated

--- iralla_eclipse/preTreatment/tools/php/Vector.php
+++ iralla_eclipse/preTreatment/tools/php/Vector.php
@@ -14,15 +14,6 @@ class Vector{
 		return $this->y;
 	}
 	
-	public function is_egal_to($other_vector){
-		if(($this->x == $other_vector->x) && ($this->y == $other_vector->y)) {
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	
 	public function __construct($var_1, $var_2 = false){
 		
 		$n = get_class($var_1);
@@ -53,39 +44,6 @@ class Vector{
 		}
 	}
 }
-
-
-//tests
-/*
-include_once 'Point.php';
-include_once 'Segment.php';
-
-$pt1 = new Point(5.9, 6.3);
-$pt2 = new Point(6, 9.2);
-
-$vector1 = new Vector(2, $pt1, $pt2);
-echo 'vector1 : ';
-print_r($vector1);
-echo "\n";
-
-$pt2 = 3;
-
-try {
-	$vector2 = new Vector(2, $pt1, $pt2);
-	echo 'ERROR : vector2 created'."\n";
-}
-catch (Exception $e) {
-	echo 'exception message : '.$e->getMessage();
-	echo "\n";
-}
-
-$pt2 = new Point(6, 9.2);
-$segment1 = new Segments($pt1, $pt2);
-$vector3 = new Vector(2, $segment1);
-echo 'vector3 : ';
-print_r($vector3);
-echo "\n";
-*/
 
 
 
