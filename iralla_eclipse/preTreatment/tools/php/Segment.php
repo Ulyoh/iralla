57942b07699b55051a95f406a9ce2d1e5488b577
ulyssey <yohann.m@gmail.com>
2011-11-21 08:56:14 -0500

    adding right left positions of endpoints in Segment class and clone pts

--- iralla_eclipse/preTreatment/tools/php/Segment.php
+++ iralla_eclipse/preTreatment/tools/php/Segment.php
@@ -4,6 +4,8 @@ include_once 'Point.php';
 class Segment{
 	private $pt1;
 	private $pt2;
+	private $left_pt;
+	private $right_pt;
 	
 	public function get_pt1(){
 		return $this->pt1;
@@ -12,6 +14,15 @@ class Segment{
 	public function get_pt2(){
 		return $this->pt2;
 	}
+	
+	public function get_left_pt(){
+		return $this->$left_pt;
+	}
+	
+	public function get_right_pt(){
+		return $this->$left_pt;
+	}
+	
 	/*
 	public function set_pt1($pt1){
 		if ($pt1 instanceof Point){
@@ -38,6 +49,22 @@ class Segment{
 	
 	public function __construct($pt1, $pt2){
 		if(($pt1 instanceof Point) && ($pt2 instanceof Point)){
+			$pt1 = clone $pt1;
+			$pt2 = clone $pt2;
+			//left and right point:
+			if(($pt1->x < $pt2->x) || (($pt1->x == $pt2->x) && ($pt1->y > $pt2->y))){
+				$pt1->position = "left";
+				$pt2->position = "right";
+				$this->left_pt = $pt1;
+				$this->right_pt = $pt2;
+			}
+			else{
+				$pt2->position = "left";
+				$pt1->position = "right";
+				$this->left_pt = $pt2;
+				$this->right_pt = $pt1;
+			}
+			
 			$this->pt1 = $pt1;
 			$this->pt2 = $pt2;
 		}
@@ -46,3 +73,10 @@ class Segment{
 		}
 	}
 }
+
+$pt1 = new Point(5.9, 6.3);
+$pt2 = new Point(6, 9.2);
+$seg = new Segment($pt1, $pt2);
+
+$pt1->x = 20000;
+echo $seg->get_pt1()->x;
