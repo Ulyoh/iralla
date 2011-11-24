515bcd500adc9cd473b2a0b4100b9242fc3f13fd
ulyssey <yohann.m@gmail.com>
2011-11-24 11:29:35 -0500

    Segment: construct accept(x1,y1,x2,y2), and tested get_left_pt and get_right_point: modify error

--- iralla_eclipse/preTreatment/tools/php/Segment.php
+++ iralla_eclipse/preTreatment/tools/php/Segment.php
@@ -1,30 +1,30 @@
 <?php
 include_once 'Point.php';
 
-class Segment{
+class Segment {
 	private $pt1;
 	private $pt2;
 	private $left_pt;
 	private $right_pt;
 	
-	public function get_pt1(){
+	public function get_pt1() {
 		return $this->pt1;
 	}
 	
-	public function get_pt2(){
+	public function get_pt2() {
 		return $this->pt2;
 	}
 	
-	public function get_left_pt(){
-		return $this->$left_pt;
+	public function get_left_pt() {
+		return $this->left_pt;
 	}
 	
-	public function get_right_pt(){
-		return $this->$right_pt;
+	public function get_right_pt() {
+		return $this->right_pt;
 	}
 	
-	public function pts_as_array(){
-		return array($this->$left_pt, $this->$right_pt);
+	public function get_pts_as_array() {
+		return array ($this->left_pt, $this->right_pt );
 	}
 	
 	/*
@@ -46,45 +46,51 @@ class Segment{
 		}
 	}
 	*/
-	public function find_intersection_with($other_segment, $return_value_if_merged = false){
-		return Point::segment_intersection(
-		$this->pt1, $this->pt2, $other_segment->pt1, $other_segment->pt2, $return_value_if_merged);
+	public function find_intersection_with($other_segment, $return_value_if_merged = false) {
+		return Point::segment_intersection ( $this->pt1, $this->pt2, $other_segment->pt1, $other_segment->pt2, $return_value_if_merged );
 	}
 	
-	public function __construct($pt1, $pt2){
-		if($pt1 == $pt2){
-			die("null segment");
-		}
-		if(($pt1 instanceof Point) && ($pt2 instanceof Point)){
-			$pt1 = clone $pt1;
-			$pt2 = clone $pt2;
-			//left and right point:
-			if(($pt1->x < $pt2->x) || (($pt1->x == $pt2->x) && ($pt1->y < $pt2->y))){
-				$pt1->position = "left";
-				$pt2->position = "right";
-				$this->left_pt = $pt1;
-				$this->right_pt = $pt2;
+	public function __construct($pt1_or_x1, $pt2_or_y1, $x2 = null, $y2 = null) {
+		if ($x2 == null && $y2 == null) {
+			$pt1 = $pt1_or_x1;
+			$pt2 = $pt2_or_y1;
+			if ($pt1 == $pt2) {
+				die ( "null segment" );
 			}
-			else{
-				$pt2->position = "left";
-				$pt1->position = "right";
-				$this->left_pt = $pt2;
-				$this->right_pt = $pt1;
+			if (($pt1 instanceof Point) && ($pt2 instanceof Point)) {
+				$pt1 = clone $pt1;
+				$pt2 = clone $pt2;
 			}
-			$this->pt1 = $pt1;
-			$this->pt2 = $pt2;
-			$pt1->segment = $this;
-			$pt2->segment = $this;
+			else {
+				throw new Exception ( '$pt1 or $pt2 is not a Point Object' );
+			}
 		}
-		else{
-			throw new Exception('$pt1 or $pt2 is not a Point Object');
+		else if (! is_numeric ( $pt1_or_x1 ) || ! is_numeric ( $pt2_or_y1 ) || ! is_numeric ( $x2 ) ||
+				 ! is_numeric ( $y2 )) {
+			throw new Exception ( 'one entry is not numeric' );
 		}
+		else {
+			$pt1 = new Point ( $pt1_or_x1, $pt2_or_y1 );
+			$pt2 = new Point ( $x2, $y2 );
+		}
+		if (($pt1->x < $pt2->x) || (($pt1->x == $pt2->x) && ($pt1->y < $pt2->y))) {
+			$pt1->position = "left";
+			$pt2->position = "right";
+			$this->left_pt = $pt1;
+			$this->right_pt = $pt2;
+		}
+		else {
+		$pt2->position = "left";
+		$pt1->position = "right";
+			$this->left_pt = $pt2;
+			$this->right_pt = $pt1;
+		}
+		$this->pt1 = $pt1;
+		$this->pt2 = $pt2;
+		$pt1->segment = $this;
+		$pt2->segment = $this;
 	}
 }
+	
+		
 
-$pt1 = new Point(5.9, 6.3);
-$pt2 = new Point(6, 9.2);
-$seg = new Segment($pt1, $pt2);
-
-$pt1->x = 20000;
-echo $seg->get_pt1()->x;
--- iralla_eclipse/preTreatment/tools/php/test_files/test_Segment.php
+++ iralla_eclipse/preTreatment/tools/php/test_files/test_Segment.php
@@ -1,5 +1,7 @@
 <?php
 include_once 'Segment.php';
+//include_once 'error.php';
+
 
 //tests
 echo "***************************************************\n";
@@ -8,25 +10,203 @@ echo "***************************************************\n";
 echo "\n";
 echo "****** with valids points:\n";
 echo "\n";
-$pt1 = new Point(5.9, 6.3);
-$pt2 = new Point(6, 9.2);
-$segment1 = new Segments($pt1, $pt2);
-print_r($segment1);
+$pt1 = new Point( 5.9, 32 );
+$pt2 = new Point( 6, 9.2 );
+$segment1 = new Segment( $pt1, $pt2 );
+print_r( $segment1 );
 echo "\n";
-echo"****** with an unvalid point:\n";
+echo "****** with an unvalid point:\n";
 echo "\n";
 $pt3 = "a string\n";
 try {
-	$segment2 = new Segments($pt1, $pt3);
+	$segment2 = new Segment( $pt1, $pt3 );
 	echo 'segment2 : ';
-	print_r($segment2);
+	print_r( $segment2 );
 	echo "\n";
-} catch (Exception $e) {
-	echo 'exception message : '.$e->getMessage();
 }
+catch ( Exception $e ) {
+	echo 'exception message : ' . $e->getMessage();
+}
+
+echo "\n";
+echo "****** with valids coordinates:\n";
+echo "\n";
+$segment1 = new Segment( 2.0, 5.3, 3, 9 );
+print_r( $segment1 );
+echo "\n";
+echo "****** with only 3 coordinates:\n";
+echo "\n";
+
+try {
+	$segment1 = new Segment( 2.0, 5.3, 3 );
+	print_r( $segment1 );
+	echo "\n";
+}
+catch ( Exception $e ) {
+	echo 'exception message : ' . $e->getMessage();
+}
+
+echo "\n";
+echo "\n";
+echo "****** with vertical segment:\n";
+$pt1 = new Point( 0, 0 );
+$pt2 = new Point( 0, 9.2 );
+$segment1 = new Segment( $pt1, $pt2 );
+print_r( $segment1 );
 
 echo "\n";
 echo "\n";
 echo "***************************************************\n";
 echo "           end test __construct():\n";
+echo "***************************************************\n";
+echo "\n";
+echo "\n";
+echo "***************************************************\n";
+echo "                  test getter:\n";
+echo "***************************************************\n";
+echo "\n";
+echo "****** with stantard segment with pt1 left and pt2 right \n";
+$pt1 = new Point( 5.9, 32 );
+$pt2 = new Point( 6, 9.2 );
+$segment1 = new Segment( $pt1, $pt2 );
+if ($segment1->get_pt1()->same_coord_as( $pt1 )) {
+	echo "get_pt1 works\n";
+}
+else {
+	die( "error with get_pt1" );
+}
+if ($segment1->get_pt2()->same_coord_as( $pt2 )) {
+	echo "get_pt2 works\n";
+}
+else {
+	die( "error with get_pt2" );
+}
+if ($segment1->get_left_pt()->same_coord_as( $pt1 )) {
+	echo "get_left_pt works\n";
+}
+else {
+	die( "error with get_left_pt" );
+}
+if ($segment1->get_right_pt()->same_coord_as( $pt2 )) {
+	echo "get_right_pt works\n";
+}
+else {
+	die( "error with get_right_pt" );
+}
+
+echo "****** with stantard segment with pt1 right and pt2 left \n";
+$pt1 = new Point( 8, 32 );
+$pt2 = new Point( 6, 9.2 );
+$segment1 = new Segment( $pt1, $pt2 );
+if ($segment1->get_pt1()->same_coord_as( $pt1 )) {
+	echo "get_pt1 works\n";
+}
+else {
+	die( "error with get_pt1" );
+}
+if ($segment1->get_pt2()->same_coord_as( $pt2 )) {
+	echo "get_pt2 works\n";
+}
+else {
+	die( "error with get_pt2" );
+}
+if ($segment1->get_left_pt()->same_coord_as( $pt2 )) {
+	echo "get_left_pt works\n";
+}
+else {
+	die( "error with get_left_pt" );
+}
+if ($segment1->get_right_pt()->same_coord_as( $pt1 )) {
+	echo "get_right_pt works\n";
+}
+else {
+	die( "error with get_right_pt" );
+}
+
+echo "****** with vertical segment with pt1 higher than pt2 \n";
+$pt1 = new Point( 8, 32 );
+$pt2 = new Point( 8, 9.2 );
+$segment1 = new Segment( $pt1, $pt2 );
+if ($segment1->get_pt1()->same_coord_as( $pt1 )) {
+	echo "get_pt1 works\n";
+}
+else {
+	die( "error with get_pt1" );
+}
+if ($segment1->get_pt2()->same_coord_as( $pt2 )) {
+	echo "get_pt2 works\n";
+}
+else {
+	die( "error with get_pt2" );
+}
+if ($segment1->get_left_pt()->same_coord_as( $pt2 )) {
+	echo "get_left_pt works\n";
+}
+else {
+	die( "error with get_left_pt" );
+}
+if ($segment1->get_right_pt()->same_coord_as( $pt1 )) {
+	echo "get_right_pt works\n";
+}
+else {
+	die( "error with get_right_pt" );
+}
+
+echo "****** with vertical segment with pt1 lower than pt2 \n";
+$pt1 = new Point( 8, 32 );
+$pt2 = new Point( 8, 54 );
+$segment1 = new Segment( $pt1, $pt2 );
+if ($segment1->get_pt1()->same_coord_as( $pt1 )) {
+	echo "get_pt1 works\n";
+}
+else {
+	die( "error with get_pt1" );
+}
+if ($segment1->get_pt2()->same_coord_as( $pt2 )) {
+	echo "get_pt2 works\n";
+}
+else {
+	die( "error with get_pt2" );
+}
+if ($segment1->get_left_pt()->same_coord_as( $pt1 )) {
+	echo "get_left_pt works\n";
+}
+else {
+	die( "error with get_left_pt" );
+}
+if ($segment1->get_right_pt()->same_coord_as( $pt2 )) {
+	echo "get_right_pt works\n";
+}
+else {
+	die( "error with get_right_pt" );
+}
+
+echo "\n";
+echo "\n";
+echo "***************************************************\n";
+echo "                end  test getter:\n";
+echo "***************************************************\n";
+echo "\n";
+echo "\n";
+echo "***************************************************\n";
+echo "                test get_pts_as_array():\n";
+echo "***************************************************\n";
+$array = $segment1->get_pts_as_array();
+if ( $array[0]->same_coord_as($pt1) &&  $array[1]->same_coord_as($pt2)){
+	echo "get_pts_as_array works";
+}
+else {
+	die( "ERROR: get_pts_as_array does not works" );
+}
+echo "\n";
+echo "\n";
+echo "***************************************************\n";
+echo "             end  test get_pts_as_array:\n";
+echo "***************************************************\n";
+echo "\n";
+echo "\n";
+echo "***************************************************\n";
+echo "***************************************************\n\n";
+echo "            test ended, no error found\n\n";
+echo "***************************************************\n";
 echo "***************************************************\n";
