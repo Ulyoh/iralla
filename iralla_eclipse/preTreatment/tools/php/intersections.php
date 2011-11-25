107a1b4a9cc2cad49736a5efd6395e8b2f28a034
ulyssey <yohann.m@gmail.com>
2011-11-25 08:59:10 -0500

    intersections verified, merged or same segment not handling

--- iralla_eclipse/preTreatment/tools/php/intersections.php
+++ iralla_eclipse/preTreatment/tools/php/intersections.php
@@ -1,117 +1,139 @@
 <?php
 include_once 'Point.php';
 include_once 'Segment.php';
+include_once 'Maillon.php';
 
 //TODO : gestion du premier mailllon de la chaine 
 
 
 //TODO : add maillon at the end of the chain not managed
 
+
 function inter_segments(array $segments) {
-	$segment = new Segment ();
-	
-	$output_list = array ();
+	$output_list = array();
 	
 	//make a list of ends points with reference to each segment endpoints
-	$endpoints_to_treat = array ();
+	$endpoints_to_treat = array();
 	foreach ( $segments as $segment ) {
-		array_merge ( $endpoints_to_treat, $segment::pts_as_array () );
+		$endpoints_to_treat = array_merge( $endpoints_to_treat, $segment->get_pts_as_array() );
 	}
 	
 	//order the endpoints_to_treat by x-axis, y-axis value of end point
 	function cmp_local($pt1, $pt2) {
-		if ($pt1 == $pt2) {
-			return 0;
+		if ($pt1->same_coord_as( $pt2 )) {
+			if ($pt1->position == "left") {
+				return - 1;
+			}
+			elseif ($pt2->position == "left") {
+				return 1;
+			}
+			else {
+				return 0;
+			}
 		}
-		if (($pt1->x < $p2->x) || (($pt1->x == $pt2) && ($pt1->y < $pt2 > y))) {
+		if (($pt1->x < $pt2->x) || (($pt1->x == $pt2->x) && ($pt1->y < $pt2->y))) {
 			return - 1;
 		}
 		else {
 			return 1;
 		}
 	}
-	usort ( $end_pts_to_treat, "cmp_local" );
 	
-	$sweep_line = array ();
-	$intersections_list = array ();
-	$first_maillon = null;
+	usort( $endpoints_to_treat, "cmp_local" );
+	
+	$intersections_list = array();
+	$sweep_line_beginning = null;
 	//while $intersections_list is not empty
-	while ( isset ( $endpoints_to_treat [0] ) == true ) {
+	while ( isset( $endpoints_to_treat[0] ) == true ) {
 		//determine the next event point in $end_pts_to_treat
-		$event = array_shift ( $endpoints_to_treat );
+		$event = array_shift( $endpoints_to_treat );
 		
-		if ($event->position == 'left') {
+		if (isset( $event->position ) && ($event->position == 'left')) {
 			
 			$seg_s_event = $event->segment;
-			$y_postion = $seg_s_event->get_left_pt ()->y;
-			$seg_s_event_and_y = array ("seg" => $seg_s_event, "y" => $y_postion );
+			$y_postion = $seg_s_event->get_left_pt()->y;
+			$seg_s_event_and_y = array( "seg" => $seg_s_event, "y" => $y_postion );
 			
 			//insert the segment in the list depending of its y position:
-			if (! isset ( $first_maillon )) {
-				$new_maillon = new Maillon ();
-				$new_maillon->cur_seg = $seg_s_event;
+			if (! isset( $sweep_line_beginning )) {
+				$new_maillon = new Maillon();
+				$new_maillon->segment = $seg_s_event;
 				$new_maillon->y = $y_postion;
 				$seg_s_event->maillon = $new_maillon;
 				$sweep_line_beginning = $new_maillon;
+			
+	//$last_maillon = $new_maillon;
 			}
 			else {
-				$new_maillon = new Maillon ();
-				$new_maillon->cur_seg = $seg_s_event;
+				$new_maillon = new Maillon();
+				$new_maillon->segment = $seg_s_event;
 				$new_maillon->y = $y_postion;
 				$seg_s_event->maillon = $new_maillon;
 				
-				$cur_maillon = $sweep_line_beginning;
-				while ( $new_maillon->y < $cur_maillon->y ) {
-					$cur_maillon = $cur_maillon->next;
+				//add the segment in a new maillon in y up order
+				if ($sweep_line_beginning->y > $new_maillon->y) {
+					Maillon::add_before( $new_maillon, $sweep_line_beginning );
 				}
-				Maillon::add_before ( $new_maillon, $cur_maillon );
+				else {
+					
+					$cur_maillon = $sweep_line_beginning;
+					$add_before_this_maillon = $cur_maillon;
+					while ( ($cur_maillon != null) && ($new_maillon->y > $cur_maillon->y) ) {
+						if ($new_maillon->y > $cur_maillon->y) {
+							$add_after_this_maillon = $cur_maillon;
+						}
+						$cur_maillon = $cur_maillon->next;
+					}
+					Maillon::add_after( $new_maillon, $add_after_this_maillon );
+				}
 				
 				//get the above and below segment:
-				$above_seg = get_above_segment ( $new_maillon ); //TODO verifiy if error of selection above below?????
-				$below_seg = get_below_segment ( $new_maillon );
+				$above_seg = get_above_segment( $new_maillon ); //TODO verifiy if error of selection above below?????
+				$below_seg = get_below_segment( $new_maillon );
 				
 				//calcul intersections with $seg_s_event if any:
-				treat_intersection_possibility ( $below_seg, $seg_s_event );
-				treat_intersection_possibility ( $seg_s_event, $above_seg );
+				treat_intersection_possibility( $below_seg, $seg_s_event, &$endpoints_to_treat );
+				treat_intersection_possibility( $seg_s_event, $above_seg, &$endpoints_to_treat );
 			}
 		}
-		elseif ($event->position == 'right') {
+		elseif (isset( $event->position ) && ($event->position == 'right')) {
 			$seg_s_event = $event->segment;
 			
 			//get the above and below segment:
-			$above_seg = $seg_s_event->next->segment; //TODO verify if error of selection above below?????
-			$below_seg = get_below_segment ( $new_maillon );
+			$above_seg = ($seg_s_event->maillon->next != null) ? $seg_s_event->maillon->next->segment : null; //TODO verify if error of selection above below?????
+			$below_seg = get_below_segment( $new_maillon );
 			
 			//remove $seg_s_event from $sweep_line:
-			$seg_s_event->maillon->remove ();
+			$seg_s_event->maillon->remove();
 			
 			//if above and below intersect not already found
-			treat_intersection_possibility ( $below_seg, $above_seg );
+			treat_intersection_possibility( $below_seg, $above_seg, &$endpoints_to_treat );
 		}
 		else { //intersection event
-			$output_list [] = $event;
-			$seg1 = $event->segment [0];
-			$seg2 = $event->segment [1];
+			$output_list[ ] = $event;
+			$seg1 = $event->segment[0];
+			$seg2 = $event->segment[1];
 			
 			//find the 2 segments below and above of $seg 1 and 2
-			$below_seg = $seg1->previous;
-			$above_seg = $seg2->next;
+			$below_seg = ($seg1->maillon->previous != null) ? $seg1->maillon->previous->segment : null;
+			$above_seg = ($seg1->maillon->next != null) ? $seg1->maillon->next->segment : null;
 			
 			//swap theirs positions:
-			$seg1->maillon->swap_with ( $seg2->maillon );
+			$seg1->maillon->swap_with( $seg2->maillon, &$sweep_line_beginning);
 			
-			//if $seg2 and $below_seg intersect not already found
-			treat_intersection_possibility ( $below_seg, $seg1 );
-			treat_intersection_possibility ( $seg2, $above_seg );
+			treat_intersection_possibility( $below_seg, $seg1, &$endpoints_to_treat );
+			treat_intersection_possibility( $seg2, $above_seg, &$endpoints_to_treat );
 		}
 	}
+	return $output_list;
 }
 
 function get_above_segment($cur_maillon) {
-	return ($new_maillon->next != null) ? $new_maillon->next->segment : null;
+	return ($cur_maillon->next != null) ? $cur_maillon->next->segment : null;
 }
 function get_below_segment($cur_maillon) {
-	$maillon_test = $cur_maillon->prev->segment;
+	//$maillon_test = ($cur_maillon->previous != null) ? $cur_maillon->previous->segment : null;
+	$maillon_test = $cur_maillon->previous;
 	while ( ($maillon_test != null) && ($cur_maillon->y == $maillon_test->y) ) {
 		$maillon_test = $maillon_test->previous;
 	}
@@ -125,49 +147,51 @@ function get_below_segment($cur_maillon) {
 
 function treat_intersection_possibility($below_seg, $above_seg, $endpoints_to_treat) {
 	if (($below_seg == null) || ($above_seg == null)) {
-		return null;
+		return;
 	}
-	if (in_array ( $below_seg, $above_seg->intersect_with )) {
+	if ((isset( $above_seg->intersect_with )) && (in_array( $below_seg, $above_seg->intersect_with ))) {
 		return;
 	}
 	
-	$intersect_result = $below_seg->find_intersection_with ( $above_seg, true );
-	if ($intersect_result [0] == true) {
-		$intersect_result [1]->segment [] = $below_seg;
-		$intersect_result [1]->segment [] = $above_seg;
-		insert_intersect_point_in_array ( $intersect_result, $endpoints_to_treat );
-		add_links_of_intersect_segments ( $below_seg, $above_seg );
+	$intersect_result = $below_seg->find_intersection_with( $above_seg, true );
+	if ($intersect_result[0] === true) {
+		$intersect_result[1]->segment[ ] = $below_seg;
+		$intersect_result[1]->segment[ ] = $above_seg;
+		insert_intersect_point_in_array( $intersect_result, &$endpoints_to_treat );
+		add_links_of_intersect_segments( $below_seg, $above_seg );
 	}
-	elseif ($intersect_result [0] == "merged") {
+	elseif ($intersect_result[0] == "merged") {
 		//TODO
 	}
-	elseif ($intersect_result [0] == "same") {
+	elseif ($intersect_result[0] == "same") {
 		//TODO
 	}
 }
 
 function add_links_of_intersect_segments($seg1, $seg2) {
-	$seg1->intersect_with [] = $seg2;
-	$seg2->intersect_with [] = $seg1;
+	$seg1->intersect_with[ ] = $seg2;
+	$seg2->intersect_with[ ] = $seg1;
 }
 
 function insert_intersect_point_in_array($intersect_result, $endpoints_to_treat) {
 	$offset = 0;
 	foreach ( $endpoints_to_treat as $value ) {
-		if ($intersect_result [1]->x < $value->x) {
+		if ($intersect_result[1]->x <= $value->x) {
 			break;
 		}
 		$offset ++;
 	}
-	$endpoints_to_treat [$offset] = $intersect_result [1];
+	//$endpoints_to_treat[$offset] = $intersect_result[1];/////////////////TO DO
+	array_splice($endpoints_to_treat, $offset, 0, "empty");
+	$endpoints_to_treat[$offset] = $intersect_result[1];
 }
 
 function get_seg_s_event_key($sweep_line, $seg2) {
 	foreach ( $sweep_line as $key => $value ) {
-		if ($value ["seg"] == $seg_s_event) {
+		if ($value["seg"] == $seg_s_event) {
 			return $key;
 		}
 	}
-	die ( "ERROR 4392" );
+	die( "ERROR 4392" );
 }		
 				
--- iralla_eclipse/preTreatment/tools/php/test_files/test_intersections.php
+++ iralla_eclipse/preTreatment/tools/php/test_files/test_intersections.php
@@ -1,4 +1,29 @@
 <?php
 include_once 'intersections.php';
+//include_once 'error.php';
 
-echo"hola";
+/*$seg_A = new Segment( 1, 9, 3, 11 );
+$seg_A->name = "A";
+$seg_B = new Segment( 2, 10, 13, 6 );
+$seg_B->name = "B";*/
+$seg_C = new Segment( 4, 2, 11, 9 );
+$seg_C->name = "C";
+$seg_D = new Segment( 6, 3, 8, 6 );
+$seg_D->name = "D";
+$seg_E = new Segment( 6, 5.5, 11, 5.5 );
+$seg_E->name = "E";
+$seg_F = new Segment( 8, 6, 8, 9 );
+$seg_F->name = "F";
+/*$seg_G = new Segment( 8, 9, 11, 1 );
+$seg_G->name = "G";
+$seg_H = new Segment( 11, 1, 11, 5 );
+$seg_H->name = "H";
+$seg_I = new Segment( 11, 6, 11, 8 );
+$seg_I->name = "I";*/
+
+//$segs = array($seg_A, $seg_B, $seg_C, $seg_D, $seg_E, $seg_F, $seg_G, $seg_H,$seg_I);
+$segs = array($seg_C, $seg_D, $seg_F, $seg_E);
+$output_list = inter_segments($segs);
+
+print_r($output_list);
+
