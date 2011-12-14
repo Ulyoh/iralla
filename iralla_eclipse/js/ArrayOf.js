4ca86d4a81ff2fab38a6a8d0026296a1179048e9
ulyssey <yohann.m@gmail.com>
2011-12-14 01:00:39 -0500

    reorganize arrayOfPolylines.removePolylinesFromIds
    Change-Id: I25d5b1d817f1dd80bdda7729f764dba77d022a04

--- iralla_eclipse/js/ArrayOf.js
+++ iralla_eclipse/js/ArrayOf.js
@@ -141,18 +141,25 @@ ArrayOfPolylines = function(){
 	
 	arrayOfPolylines.removePolylinesFromIds = function(idsArray){
 		mainLoop: for(var i = arrayOfPolylines.length-1; i >= 0 ; i--){
-			for(var j = 0; j < idsArray.length; j++){
-				if (arrayOfPolylines[i].id == idsArray[j]) {
-					if (arrayOfPolylines[i].selected != true){
-						arrayOfPolylines[i].removeBusline();
-						arrayOfPolylines.splice(i, 0);
-					}
-					if(idsArray.length <= 0){
-						break mainLoop;
-					}
-					break;
+			if (arrayOfPolylines[i].selected != true){
+				if ((arrayOfPolylines[i].type == 'mainLine') || (arrayOfPolylines[i].type == 'feeder')){
+					arrayOfPolylines[i].removeBusline();
+				}
+				else{
+					for(var j = 0; j < idsArray.length; j++){
+						if (arrayOfPolylines[i].id == idsArray[j]) {
+							arrayOfPolylines[i].removeBusline();
+							arrayOfPolylines.splice(i, 0);
+							/*if(idsArray.length <= 0){
+								break mainLoop;
+							}*/
+							break;
+						}
+				}
 				}
 			}
+			
+			
 		}
 	};
 
