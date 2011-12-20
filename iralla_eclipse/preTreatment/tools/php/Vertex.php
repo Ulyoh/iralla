376ab7729a7647068149f403c74e3cbcf0ed1389
ulyssey <yohann.m@gmail.com>
2011-12-20 15:30:47 -0500

    Vertex: adding $this->with_multiplicador = true bu default
    Change-Id: I31c03d3697a25099cd97cbe97346f57563e3250f

--- iralla_eclipse/preTreatment/tools/php/Vertex.php
+++ iralla_eclipse/preTreatment/tools/php/Vertex.php
@@ -2,6 +2,7 @@
 class Vertex{
 	public $lat;
 	public $lng;
+	public $with_multiplicador;
 	
 	public static function are_egal($vertex_1, $vertex_2){
 		if(($vertex_1->lat == $vertex_2->lat) 
@@ -13,8 +14,9 @@ class Vertex{
 		}
 	}
 	
-	public function __construct($lat_lng_string_or_lat, $lng_optional){
+	public function __construct($lat_lng_string_or_lat, $lng_optional, $with_multiplicador = true){
 		global $multipicador;
+		$this->with_multiplicador = $with_multiplicador;
 		if($multipicador == null){
 			$multipicador = 10000000;
 		}
@@ -22,29 +24,51 @@ class Vertex{
 		if ((is_string($lat_lng_string_or_lat) == true)
 		&& ( $lng_optional == NULL)){
 			$buffer = explode(" ", $lat_lng_string_or_lat);
-			$this->lat = bcmul($buffer[0], $multipicador);
-			$this->lng = bcmul($buffer[1], $multipicador);
+			if ($this->with_multiplicador === true){
+				$this->lat = bcmul($buffer[0], $multipicador);
+				$this->lng = bcmul($buffer[1], $multipicador);
+			}
+			else{
+				$this->lat = $buffer[0];
+				$this->lng = $buffer[1];
+			}
 		}
 		else{
-			$this->lat = bcmul($lat_lng_string_or_lat, $multipicador);
-			$this->lng = bcmul($lng_optional, $multipicador);
+			if ($this->with_multiplicador === true){
+				$this->lat = bcmul($lat_lng_string_or_lat, $multipicador);
+				$this->lng = bcmul($lng_optional, $multipicador);
+			}
+			else{
+				$this->lat = $lat_lng_string_or_lat;
+				$this->lng = $lng_optional;
+			}
 		}
 	}
 	
 	public function get_lat(){
-		global $multipicador;
-		if($multipicador == null){
-			$multipicador = 10000000;
+		if ($this->with_multiplicador === true){
+			global $multipicador;
+			if($multipicador == null){
+				$multipicador = 10000000;
+			}
+			return bcdiv($this->lat, $multipicador, 9);
 		}
-		return bcdiv($this->lat, $multipicador, 9);
+		else{
+			return $this->lat;
+		}
 	}
 	
 	public function get_lng(){
-		global $multipicador;
-		if($multipicador == null){
-			$multipicador = 10000000;
+		if ($this->with_multiplicador === true){
+			global $multipicador;
+			if($multipicador == null){
+				$multipicador = 10000000;
+			}
+			return bcdiv($this->lng, $multipicador, 9);
 		}
-		return bcdiv($this->lng, $multipicador, 9);
+		else{
+			return $this->lng;
+		}
 	}
 }
 
