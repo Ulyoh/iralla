e1be7dc51af0e733736180339374e07c35085b49
ulyssey <yohann.m@gmail.com>
2011-12-29 14:04:19 -0500

    correction of from first vertex
    Change-Id: I17416a9ac16abce3abd8788e3fc886080f994272

--- iralla_eclipse/preTreatment/mySQL/create_grid/distances_to_links.php
+++ iralla_eclipse/preTreatment/mySQL/create_grid/distances_to_links.php
@@ -17,7 +17,7 @@ function distances_to_links($busline, $previous_link, $next_link, $current_area,
 		+ real_distance_between_2_vertex($busline['path'][$current_out_coords['prev_index_before_intersection']], $current_out_coords['intersection'], $multipicador);
 	
 	$distances = array();
-	$square_to_save['distance_from_first_vertex'] = $distance_from_first_vertex;
+	$distances['from_first_vertex'] = $distance_from_first_vertex;
 	$distances['from_previous_link'] = $distance_from_first_vertex - $previous_link['distance_from_first_vertex'];
 	$distances['to_next_link'] = $next_link['distance_from_first_vertex'] - $distance_from_first_vertex;
 	
