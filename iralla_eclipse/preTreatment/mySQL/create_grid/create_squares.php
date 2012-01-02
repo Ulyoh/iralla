cf922127c2d34f31c237d74b632b6622eb58e3de
ulyssey <yohann.m@gmail.com>
2012-01-02 14:29:58 -0500

    create_squares adapt to new 'squares' db
    Change-Id: If40dbec9c273c8c209ca6e948501b7ed190a6c15

--- iralla_eclipse/preTreatment/mySQL/create_grid/create_squares.php
+++ iralla_eclipse/preTreatment/mySQL/create_grid/create_squares.php
@@ -13,17 +13,16 @@ function create_squares($bus_line,
 	$square_to_save['bl_name'] = $bus_line['bus_line_name'];
 	$square_to_save['lat'] = $current_out_coords['next_square']->lat / $grid_path;
 	$square_to_save['lng'] = $current_out_coords['next_square']->lng / $grid_path;
-	$square_to_save['pt_coords'] = $current_out_coords['intersection']->get_lat_lng_string();
+	$square_to_save['pt_coords_lat'] = $current_out_coords['intersection']->get_lat();
+	$square_to_save['pt_coords_lng'] = $current_out_coords['intersection']->get_lng();
 	//prev_index_of_pt or the index of the vertex if they are merged
 	$square_to_save['prev_index_of_pt'] = $current_out_coords['prev_index_before_intersection'];
-	$square_to_save['prev_vertex_of_prev_link'] = $previous_link['prevIndex'];
-	$square_to_save['prev_vertex_of_next_link'] = $next_link['prevIndex'];;
-	$square_to_save['prev_link_coords'] = json_encode(array(
-			'lat' => $previous_link['lat'],
-			'lng' => $previous_link['lng']));
-	$square_to_save['next_link_coords'] = json_encode(array(
-			'lat' => $next_link['lat'],
-			'lng' => $next_link['lng']));
+	$square_to_save['prev_index_of_prev_link'] = $previous_link['prevIndex'];
+	$square_to_save['prev_index_of_next_link'] = $next_link['prevIndex'];;
+	$square_to_save['prev_link_coords_lat'] = $previous_link['lat'];
+	$square_to_save['prev_link_coords_lng'] = $previous_link['lng'];
+	$square_to_save['next_link_coords_lat'] = $next_link['lat'];
+	$square_to_save['next_link_coords_lat'] = $next_link['lng'];
 	$square_to_save['prev_bs_linked_id'] = $previous_link['busStationId'];
 	$square_to_save['next_bs_linked_id'] = $next_link['busStationId'];
 	$square_to_save['distance_to_prev_link'] = $distances['from_previous_link'];
@@ -31,7 +30,20 @@ function create_squares($bus_line,
 	$square_to_save['distance_from_first_vertex'] = $distances['from_first_vertex'];
 	$square_to_save['previous_link_id'] = $previous_link['id'];
 	$square_to_save['next_link_id'] = $next_link['id'];
-	$square_to_save['flows'] = $bus_line['flows'];
+	
+	switch($bus_line['flows']){
+		case 'normal':
+			$square_to_save['flows'] = 1;
+			break;
+			
+		case 'reverse':
+			$square_to_save['flows'] = 2;
+			break;
+
+		case 'both':
+			$square_to_save['flows'] = 3;
+			break;		
+	}
 	
 	$links_squares[] = $square_to_save;
 }
