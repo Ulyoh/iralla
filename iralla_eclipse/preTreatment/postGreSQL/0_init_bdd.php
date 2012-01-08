966880766879abde05a9348b30a32f1fc57971a0
ulyssey <yohann.m@gmail.com>
2012-01-08 12:46:15 -0600

    adapting init_bdd to postgreSQL
    Change-Id: Id3e129ca6df04d72aa9adcdcd27ad8880ca2d2cb

--- iralla_eclipse/preTreatment/postGreSQL/0_init_bdd.php
+++ iralla_eclipse/preTreatment/postGreSQL/0_init_bdd.php
@@ -4,15 +4,15 @@ require_once 'access_to_postgreSQL.php';
 	
 	//create the table of links if not exist
 	$query = 'CREATE TABLE IF NOT EXISTS links (
-            id SERIAL PRIMARY KEY,					--mySQL : UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-            busStationId INTEGER NOT NULL,			--mySQL : MEDIUMINT(6) UNSIGNED,
-            busLineId INTEGER NOT NULL,				--mySQL : MEDIUMINT(6) UNSIGNED,
-            prevIndex SMALLINT NOT NULL,			--mySQL : SMALLINT(4) UNSIGNED,
-            distanceToPrevIndex SMALLINT NOT NULL, 	--mySQL : MEDIUMINT(6),
-            lat FLOAT(35) NOT NULL, 				--mySQL : FLOAT(12,10),
-            lng FLOAT(35) NOT NULL, 				--mySQL : FLOAT(12,10),
-            inUse BOOL DEFAULT true NOT NULL		--mySQL : inUse BOOL DEFAULT 1,
-           											--mySQL : INDEX busLineId (busLineId, prevIndex, distanceToPrevIndex)
+            id SERIAL PRIMARY KEY,					--mySQL UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+            busStationId INTEGER NOT NULL,			--mySQL MEDIUMINT(6) UNSIGNED,
+            busLineId INTEGER NOT NULL,				--mySQL MEDIUMINT(6) UNSIGNED,
+            prevIndex SMALLINT NOT NULL,			--mySQL SMALLINT(4) UNSIGNED,
+            distanceToPrevIndex SMALLINT NOT NULL, 	--mySQL MEDIUMINT(6),
+            lat FLOAT(35) NOT NULL, 				--mySQL FLOAT(12,10),
+            lng FLOAT(35) NOT NULL, 				--mySQL FLOAT(12,10),
+            inUse BOOL DEFAULT true NOT NULL		--mySQL inUse BOOL DEFAULT 1,
+           											--mySQL INDEX busLineId (busLineId, prevIndex, distanceToPrevIndex)
             );';
 
 	
@@ -26,22 +26,20 @@ require_once 'access_to_postgreSQL.php';
 	
 	create_index('busLineId_idx', 'busLineId, prevIndex, distanceToPrevIndex');
 
-	return;
-	
 	//create the table of bus stations if not exist
 	$query = "CREATE TABLE IF NOT EXISTS bus_stations (
-            id MEDIUMINT(6) PRIMARY KEY AUTO_INCREMENT,
-            type VARCHAR(20),
-            name TINYTEXT,
-            lat FLOAT(12,10),
-            lng FLOAT(12,10),
-            circleCenterLat FLOAT(12,10),
-            circleCenterLng FLOAT(12,10),
-            circleRadius SMALLINT,
-            linksListIds TEXT,
-            layerId MEDIUMINT(6),
-            inUse BOOL DEFAULT 0,
-            idFromJavascript MEDIUMINT(6)
+			id SERIAL PRIMARY KEY,		            --mySQL id MEDIUMINT(6) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+			type VARCHAR(20),			            --mySQL type VARCHAR(20),
+			name VARCHAR(100),						--mySQL name TINYTEXT,
+			lat FLOAT(35) NOT NULL,		            --mySQL lat FLOAT(12,10),
+			lng FLOAT(35) NOT NULL, 	            --mySQL lng FLOAT(12,10),
+			circleCenterLat	FLOAT(35) NOT NULL,		--mySQL circleCenterLat FLOAT(12,10),
+			circleCenterLng	FLOAT(35) NOT NULL,		--mySQL circleCenterLng FLOAT(12,10),
+			circleRadius  SMALLINT NOT NULL,		--mySQL circleRadius SMALLINT UNSIGNED,
+			linksListIds TEXT,			            --mySQL linksListIds TEXT,
+			layerId INTEGER NOT NULL,				--mySQL layerId MEDIUMINT(6) UNSIGNED,
+			inUse BOOL DEFAULT true NOT NULL,		--mySQL inUse BOOL DEFAULT 0,
+			idFromJavascript INTEGER NOT NULL		--mySQL idFromJavascript MEDIUMINT(6)
             )";
 
 	if (($bdd->exec($query)) === false){
@@ -54,21 +52,23 @@ require_once 'access_to_postgreSQL.php';
 	
 	//create the table of bus lines if not exist
 	$query = "CREATE TABLE IF NOT EXISTS bus_lines (
-            id MEDIUMINT(6) PRIMARY KEY AUTO_INCREMENT,
-            name TINYTEXT,
-            layerId MEDIUMINT(6),
-            layerName TINYTEXT,
-            type ENUM('by_foot','mainLine','feeder','other'),
-            color VARCHAR(6),
-            path TEXT,
-            flows TINYTEXT,
-            boundariesListId TINYTEXT,
-            pathsAreaOfBusStations TEXT,
-            areaOnlyBusStations TEXT,
-            busStationsIdsList TEXT,
-            connectionsIdsList TEXT,
-            inUse BOOL DEFAULT 0,
-            idFromJavascript MEDIUMINT(6)
+			id SERIAL PRIMARY KEY,		            --mySQL id MEDIUMINT(6) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+			name VARCHAR(100),			            --mySQL name TINYTEXT,
+			layerId INTEGER NOT NULL,				--mySQL layerId MEDIUMINT(6) UNSIGNED,
+			layerName VARCHAR(100),		            --mySQL layerName TINYTEXT,
+			type VARCHAR(8),			            --mySQL type ENUM('by_foot','mainLine','feeder','other'),
+			CHECK (type IN ('by_foot','mainLine','feeder','other')),
+			color VARCHAR(6),			            --mySQL color VARCHAR(6),
+			path TEXT,					            --mySQL path TEXT,
+			flows SMALLINT,				            --mySQL flows TINYTEXT,
+			CHECK ( 0 <= flows AND flows <= 3),
+			boundariesListId VARCHAR(255),			--mySQL boundariesListId TINYTEXT,
+			pathsAreaOfBusStations TEXT,			--mySQL pathsAreaOfBusStations TEXT,
+			areaOnlyBusStations TEXT,				--mySQL areaOnlyBusStations TEXT,
+			busStationsIdsList TEXT,				--mySQL busStationsIdsList TEXT,
+			connectionsIdsList TEXT,				--mySQL connectionsIdsList TEXT,
+			inUse BOOL DEFAULT false NOT NULL,		--mySQL inUse BOOL DEFAULT 0,
+			idFromJavascript INTEGER NOT NULL		--mySQL idFromJavascript MEDIUMINT(6)
             )";
 
 	if (($bdd->exec($query)) === false){
@@ -78,14 +78,15 @@ require_once 'access_to_postgreSQL.php';
 	else{
 		echo "table bus_lines created or already exists\n";
 	}
-
+	
+	
 	//create the table of arrows if not exist
 	$query = "CREATE TABLE IF NOT EXISTS arrows (
-            id MEDIUMINT(6) PRIMARY KEY AUTO_INCREMENT,
-            name TINYTEXT,
-            layerId MEDIUMINT(6),
-            color VARCHAR(6),
-            path TEXT
+            id SERIAL PRIMARY KEY,		            --mySQL id MEDIUMINT(6) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+            name VARCHAR(100),			            --mySQL name TINYTEXT,
+            layerId INTEGER NOT NULL,				--mySQL layerId MEDIUMINT(6) UNSIGNED,
+            color VARCHAR(6),						--mySQL color VARCHAR(6),
+            path TEXT								--mySQL path TEXT
             )";
 
 	if (($bdd->exec($query)) === false){
@@ -96,40 +97,42 @@ require_once 'access_to_postgreSQL.php';
 		echo "table arrows created or already exists\n";
 	}
 	
+	return;
+	
 	//create the table of connections between nearest bus stations if not exist
 	$query = "CREATE TABLE IF NOT EXISTS nearest_connected_bus_stations (
-            id MEDIUMINT(6) PRIMARY KEY AUTO_INCREMENT,
+            id SERIAL PRIMARY KEY,		            					--mySQL MEDIUMINT(6) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
             
-            busStationIdDeparture MEDIUMINT(6),
-            busStationNameDeparture TINYTEXT,
-            busStationLatDeparture FLOAT(12,10),
-            busStationLngDeparture FLOAT(12,10),
-            busStationTypeDeparture VARCHAR(20),
+            busStationIdDeparture INTEGER NOT NULL						--mySQL MEDIUMINT(6) UNSIGNED,
+            busStationNameDeparture VARCHAR(100)						--mySQL TINYTEXT,
+            busStationLatDeparture FLOAT(35) NOT NULL,					--mySQL FLOAT(12,10),
+            busStationLngDeparture FLOAT(35) NOT NULL,					--mySQL FLOAT(12,10),
+            busStationTypeDeparture VARCHAR(20),						--mySQL VARCHAR(20),
             
-            linkIdDeparture MEDIUMINT(6),
-            linkPrevIndexDeparture SMALLINT(4),
-            linkDistanceToPrevIndexDeparture MEDIUMINT(6),
-            linkLatDeparture FLOAT(12,10),
-            linkLngDeparture FLOAT(12,10),
+            linkIdDeparture INTEGER NOT NULL							--mySQL MEDIUMINT(6) UNSIGNED,
+            linkPrevIndexDeparture SMALLINT NOT NULL,					--mySQL SMALLINT(4) UNSIGNED,
+            linkDistanceToPrevIndexDeparture INTEGER NOT NULL			--mySQL MEDIUMINT(6) UNSIGNED,
+            linkLatDeparture FLOAT(35) NOT NULL,		            	--mySQL FLOAT(12,10),
+            linkLngDeparture FLOAT(35) NOT NULL,		            	--mySQL FLOAT(12,10),
             
-            nextLinkId MEDIUMINT(6),
-            nextLinkPrevIndex SMALLINT(4),
-            nextLinkDistanceToPrevIndex MEDIUMINT(6),
-            nextLinkLat FLOAT(12,10),
-            nextLinkLng FLOAT(12,10),
+            nextLinkId INTEGER NOT NULL									--mySQL MEDIUMINT(6) UNSIGNED,
+            nextLinkPrevIndex SMALLINT NOT NULL,						--mySQL SMALLINT(4) UNSIGNED,
+            nextLinkDistanceToPrevIndex INTEGER NOT NULL				--mySQL MEDIUMINT(6) UNSIGNED,
+            nextLinkLat FLOAT(35) NOT NULL,		            			--mySQL FLOAT(12,10),
+            nextLinkLng FLOAT(35) NOT NULL,		            			--mySQL FLOAT(12,10),
             
-            nextBusStationId MEDIUMINT(6),
-            nextBusStationName TINYTEXT,
-            nextBusStationLat FLOAT(12,10),
-            nextBusStationLng FLOAT(12,10),
-            nextBusStationType VARCHAR(20),
+            nextBusStationId INTEGER NOT NULL							--mySQL MEDIUMINT(6) UNSIGNED,
+            nextBusStationName VARCHAR(100),			        		--mySQL TINYTEXT,
+            nextBusStationLat FLOAT(35) NOT NULL,		            	--mySQL FLOAT(12,10),
+            nextBusStationLng FLOAT(35) NOT NULL,		            	--mySQL FLOAT(12,10),
+            nextBusStationType VARCHAR(20),								--mySQL VARCHAR(20),
             
-            busLineId MEDIUMINT(6),
-            busLineName TINYTEXT,
-            busLinePath TEXT,
-            busLineType ENUM('none','mainLine','feeder','other'),
-            
-            length MEDIUMINT(6),
+            busLineId INTEGER NOT NULL									--mySQL MEDIUMINT(6) UNSIGNED,
+            busLineName VARCHAR(100),			            			--mySQL TINYTEXT,
+            busLinePath TEXT,											--mySQL TEXT,
+            busLineType VARCHAR(8),										--mySQL ENUM('none','mainLine','feeder','other'),
+            CHECK busLineType IN ('none','mainLine','feeder','other'),
+            length  INTEGER NOT NULL									--mySQL MEDIUMINT(6) UNSIGNED,
             
             INDEX busStationIdDeparture (busStationIdDeparture),
             INDEX nextLinkId (nextLinkId)
@@ -146,15 +149,15 @@ require_once 'access_to_postgreSQL.php';
 	
 	//create the table of connections between all bus stations if not exist
 	$query = "CREATE TABLE IF NOT EXISTS bus_stations_to_bus_stations (
-            id MEDIUMINT(6) PRIMARY KEY AUTO_INCREMENT,
-            start_bus_station_id MEDIUMINT(6),
-            start_lat  FLOAT(12,10),
-            start_lng  FLOAT(12,10),
-            end_bus_station_id MEDIUMINT(6),
-            end_lat  FLOAT(12,10),
-            end_lng  FLOAT(12,10),
-            road_datas LONGTEXT,
-            time MEDIUMINT(6),
+            id SERIAL PRIMARY KEY,		            		--mySQL id MEDIUMINT(6) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+            start_bus_station_id INTEGER NOT NULL			--mySQL MEDIUMINT(6) UNSIGNED,
+            start_lat FLOAT(35) NOT NULL,		            --mySQL  FLOAT(12,10),
+            start_lng FLOAT(35) NOT NULL,		            --mySQL  FLOAT(12,10),
+            end_bus_station_id INTEGER NOT NULL				--mySQL MEDIUMINT(6) UNSIGNED,
+            end_lat FLOAT(35) NOT NULL,		            	--mySQL  FLOAT(12,10),
+            end_lng FLOAT(35) NOT NULL,		           		--mySQL  FLOAT(12,10),
+            road_datas LONGTEXT,							--mySQL TEXT,
+            time INTEGER NOT NULL							--mySQL MEDIUMINT(6) UNSIGNED,
             INDEX start_lat_lng (start_lat, start_lng),
             INDEX end_lat_lng (end_lat, end_lng),
             UNIQUE INDEX couple (start_bus_station_id, end_bus_station_id)
@@ -170,23 +173,23 @@ require_once 'access_to_postgreSQL.php';
 		
 	//create the table of connections between all bus stations if not exist
 	$query = "CREATE TABLE IF NOT EXISTS to_square (
-			id MEDIUMINT(6) PRIMARY KEY AUTO_INCREMENT,
-			bus_line_id MEDIUMINT(6),
-			bus_line_name TINYTEXT,
-			lat INT,
-			lng INT,
-			from_link_lat INT(9),
-			from_link_lng INT(9),
-			go_in_point_lat MEDIUMINT(6),
-			go_in_point_lng MEDIUMINT(6),
-			path TEXT,
-			previous_index_of_go_out SMALLINT,
-			previous_vertex_of_link SMALLINT,
-			length INT,
-			id_of_bus_station_linked MEDIUMINT(6),
-			link_id MEDIUMINT(6) ,
-			previous_link_id MEDIUMINT(6) ,
-			next_link_id MEDIUMINT(6) ,
+			id SERIAL PRIMARY KEY,		            			--mySQL id MEDIUMINT(6) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+			bus_line_id INTEGER NOT NULL						--mySQL MEDIUMINT(6) UNSIGNED,
+			bus_line_name VARCHAR(100),			            	--mySQL TINYTEXT,
+			lat INTEGER											--mySQL INT,
+			lng INTEGER											--mySQL INT,
+			from_link_lat INTEGER								--mySQL INT(9),
+			from_link_lng INTEGER								--mySQL INT(9),
+			go_in_point_lat INTEGER NOT NULL					--mySQL MEDIUMINT(6) UNSIGNED,
+			go_in_point_lng INTEGER NOT NULL					--mySQL MEDIUMINT(6) UNSIGNED,
+			path TEXT,											--mySQL TEXT,
+			previous_index_of_go_out SMALLINT NOT NULL,			--mySQL SMALLINT UNSIGNED,
+			previous_vertex_of_link SMALLINT NOT NULL,			--mySQL SMALLINT UNSIGNED,
+			length INTEGER										--mySQL INT,
+			id_of_bus_station_linked INTEGER NOT NULL			--mySQL MEDIUMINT(6) UNSIGNED,
+			link_id INTEGER NOT NULL							--mySQL MEDIUMINT(6) UNSIGNED ,
+			previous_link_id INTEGER NOT NULL					--mySQL MEDIUMINT(6) UNSIGNED ,
+			next_link_id INTEGER NOT NULL						--mySQL MEDIUMINT(6) UNSIGNED ,
 			INDEX lat_lng (lat,lng),
 			INDEX bus_line_id (bus_line_id)
             )";
@@ -201,20 +204,20 @@ require_once 'access_to_postgreSQL.php';
 
 		//create the table of connections between all bus stations if not exist
 	$query = "CREATE TABLE IF NOT EXISTS from_square (
-			id MEDIUMINT(6) PRIMARY KEY AUTO_INCREMENT,
-			bus_line_id MEDIUMINT(6),
-			bus_line_name TINYTEXT,
-			lat INT,
-			lng INT,
-			go_out_point_lat MEDIUMINT(6),
-			go_out_point_lng MEDIUMINT(6),
-			to_link_lat INT(9),
-			to_link_lng INT(9),
-			path TEXT,
-			from_index SMALLINT,
-			to_index SMALLINT,
-			length INT,
-			id_of_bus_station_linked MEDIUMINT(6),
+			id SERIAL PRIMARY KEY,		            			--mySQL id MEDIUMINT(6) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+			bus_line_id INTEGER NOT NULL						--mySQL MEDIUMINT(6) UNSIGNED,
+			bus_line_name VARCHAR(100),			           		--mySQL TINYTEXT,
+			lat INTEGER											--mySQL INT,
+			lng INTEGER											--mySQL INT,
+			go_out_point_lat INTEGER NOT NULL					--mySQL MEDIUMINT(6) UNSIGNED,
+			go_out_point_lng INTEGER NOT NULL					--mySQL MEDIUMINT(6) UNSIGNED,
+			to_link_lat INTEGER									--mySQL INT(9),
+			to_link_lng INTEGER									--mySQL INT(9),
+			path TEXT,											--mySQL TEXT,
+			from_index SMALLINT NOT NULL,						--mySQL SMALLINT UNSIGNED,
+			to_index SMALLINT NOT NULL,							--mySQL SMALLINT UNSIGNED,
+			length INTEGER										--mySQL INT,
+			id_of_bus_station_linked INTEGER NOT NULL			--mySQL MEDIUMINT(6) UNSIGNED,
 			INDEX lat_lng (lat,lng)
             )";
 
@@ -229,25 +232,26 @@ require_once 'access_to_postgreSQL.php';
 	####################################"""
 	//create the table of connections between all bus stations if not exist
 	$query = "CREATE TABLE IF NOT EXISTS squares (
-		id MEDIUMINT(6) PRIMARY KEY AUTO_INCREMENT,
-		bl_id MEDIUMINT(6),
-		bl_name TINYTEXT,
-		lat INT,
-		lng INT,
-		pt_coords VARCHAR(50),
-		prev_index_of_pt SMALLINT,
-		prev_vertex_of_prev_link SMALLINT,
-		prev_vertex_of_next_link SMALLINT,
-		prev_link_coords VARCHAR(50),
-		next_link_coords VARCHAR(50),
-		prev_bs_linked_id MEDIUMINT(6),
-		next_bs_linked_id MEDIUMINT(6),
-		distance_to_prev_link INT,
-		distance_to_next_link INT,
-		distance_from_first_vertex INT,
-		previous_link_id MEDIUMINT(6),
-		next_link_id MEDIUMINT(6),
-		flows TINYTEXT
+		id SERIAL PRIMARY KEY,		            				--mySQL id MEDIUMINT(6) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+		bl_id INTEGER NOT NULL									--mySQL MEDIUMINT(6) UNSIGNED,
+		bl_name VARCHAR(100),			            			--mySQL TINYTEXT,
+		lat INTEGER												--mySQL INT,
+		lng INTEGER												--mySQL INT,
+		pt_coords VARCHAR(50),									--mySQL VARCHAR(50),
+		prev_index_of_pt SMALLINT NOT NULL,						--mySQL SMALLINT UNSIGNED,
+		prev_vertex_of_prev_link SMALLINT NOT NULL,				--mySQL SMALLINT UNSIGNED,
+		prev_vertex_of_next_link SMALLINT NOT NULL,				--mySQL SMALLINT UNSIGNED,
+		prev_link_coords VARCHAR(50),							--mySQL VARCHAR(50),
+		next_link_coords VARCHAR(50),							--mySQL VARCHAR(50),
+		prev_bs_linked_id INTEGER NOT NULL						--mySQL MEDIUMINT(6) UNSIGNED,
+		next_bs_linked_id INTEGER NOT NULL						--mySQL MEDIUMINT(6) UNSIGNED,
+		distance_to_prev_link INTEGER							--mySQL INT,
+		distance_to_next_link INTEGER							--mySQL INT,
+		distance_from_first_vertex INTEGER						--mySQL INT,
+		previous_link_id INTEGER NOT NULL						--mySQL MEDIUMINT(6) UNSIGNED,
+		next_link_id INTEGER NOT NULL							--mySQL MEDIUMINT(6) UNSIGNED,
+		flows SMALLINT,				            				--mySQL flows TINYTEXT,
+		CHECK ( 0 <= flows AND flows <= 3),
 	)";
 	
 	if (($bdd->exec($query)) === false){
@@ -262,9 +266,9 @@ require_once 'access_to_postgreSQL.php';
 	
 	//create the table of text to search of rutas
 	$query = "CREATE TABLE IF NOT EXISTS words_to_search_rutas (
-			id MEDIUMINT(4) PRIMARY KEY AUTO_INCREMENT,
-			bus_line_id MEDIUMINT(6),
-			word VARCHAR(10),
+			id SERIAL PRIMARY KEY,		            			--mySQL id MEDIUMINT(4) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+			bus_line_id INTEGER NOT NULL						--mySQL MEDIUMINT(6) UNSIGNED,
+			word VARCHAR(10),									--mySQL VARCHAR(10),
 			INDEX word (word)
             )";
 
@@ -279,12 +283,12 @@ require_once 'access_to_postgreSQL.php';
 	
 	//create the table of geolocalisation
 	$query = "CREATE TABLE IF NOT EXISTS geolocalisation (
-			id MEDIUMINT(8) PRIMARY KEY AUTO_INCREMENT,
-			way_name VARCHAR(150),
-			way_name_id MEDIUMINT(8),
-			reference BIGINT,
-			lat VARCHAR(12),
-			lng VARCHAR(12),
+			id SERIAL PRIMARY KEY,		            			--mySQL id MEDIUMINT(8) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+			way_name VARCHAR(150),								--mySQL VARCHAR(150),
+			way_name_id INTEGER NOT NULL						--mySQL MEDIUMINT(8) UNSIGNED,
+			reference BIGINT NOT NULL							--mySQL BIGINT UNSIGNED,
+			lat VARCHAR(12),									--mySQL VARCHAR(12),
+			lng VARCHAR(12),									--mySQL VARCHAR(12),
 			INDEX way_name_id (way_name_id),
 			INDEX reference (reference)
             )";
@@ -299,10 +303,10 @@ require_once 'access_to_postgreSQL.php';
 	
 		//create the table of words in way names
 	$query = "CREATE TABLE IF NOT EXISTS geo_words_in_ways_names (
-			id MEDIUMINT(8) PRIMARY KEY AUTO_INCREMENT,
-			word VARCHAR(40),
-			way_name_id MEDIUMINT(8),
-			nth TINYINT,
+			id SERIAL PRIMARY KEY,		            			--mySQL id MEDIUMINT(8) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+			word VARCHAR(40),									--mySQL VARCHAR(40),
+			way_name_id INTEGER NOT NULL						--mySQL MEDIUMINT(8),
+			nth	SMALLINT,										--mySQL TINYINT UNSIGNED,
 			INDEX word (word),
 			INDEX way_name_id (way_name_id)
             )";
@@ -317,12 +321,12 @@ require_once 'access_to_postgreSQL.php';
 
 		//create the table of ways names
 	$query = "CREATE TABLE IF NOT EXISTS geo_ways_names (
-			id INTEGER(8) PRIMARY KEY AUTO_INCREMENT,
-			way_name VARCHAR(150),
-			way_name_reduced VARCHAR(150),
-			words_selected_to_search VARCHAR(180),
-			word_quantity_to_search SMALLINT,
-			geolocalisation_ids INTEGER -- was MEDIUMTEXT in mySQL
+			id SERIAL PRIMARY KEY,		           				--mySQL id MEDIUMINT(8) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+			way_name VARCHAR(150),								--mySQL VARCHAR(150),
+			way_name_reduced VARCHAR(150),						--mySQL VARCHAR(150),
+			words_selected_to_search VARCHAR(180),				--mySQL VARCHAR(180),
+			word_quantity_to_search	SMALLINT,					--mySQL TINYINT UNSIGNED,
+			geolocalisation_ids	INTEGER							--mySQL  MEDIUMTEXT ???seems that was an error
             )";
 
 	if (($bdd->exec($query)) === false){
