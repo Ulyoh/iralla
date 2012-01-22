b326cc8b491bbc2ec008549c9531a797c1a61179
ulyssey <yohann.m@gmail.com>
2012-01-22 17:01:46 -0600

    remove flow column and restrict the selected columns to be shown
    Change-Id: I38c9c5a831f895ac7eebc35e021e2d291598ad4b

--- iralla_eclipse/mySQL/find_communs_bs.sql
+++ iralla_eclipse/mySQL/find_communs_bs.sql
@@ -1,4 +1,16 @@
-select * from (
+select 
+	start_min_previous_link_id,
+	start_max_previous_link_id,
+	start_min_next_link_id,
+	start_max_next_link_id,
+	start_busLineId,
+	end_busStationId as intermediate_bs_id,
+	end_min_previous_link_id,
+	end_max_previous_link_id,
+	end_min_next_link_id,
+	end_max_next_link_id,
+	end_busLineId	
+	from (
 select start_bls.start_min_previous_link_id,
 	start_bls.start_max_previous_link_id,
 	start_bls.start_min_next_link_id,
@@ -10,7 +22,6 @@ select start_bls.start_min_previous_link_id,
 	end_links.end_max_previous_link_id,
 	end_links.end_min_next_link_id,
 	end_links.end_max_next_link_id,
-	end_links.flows,
 	end_links.end_busLineId,
 	end_links.end_busStationId
 from start_bls
@@ -18,9 +29,8 @@ inner join links on links.busLineId = start_bls.bl_id
 inner join (
 	select end_bls.end_min_previous_link_id,
 	end_bls.end_max_previous_link_id,
-end_bls.end_min_next_link_id,
+	end_bls.end_min_next_link_id,
 	end_bls.end_max_next_link_id,
-	end_bls.flows ,
 	links.id as start_id,
 	links.busLineId as end_busLineId,
 	links.busStationId as end_busStationId
@@ -29,4 +39,3 @@ end_bls.end_min_next_link_id,
 )end_links
 on links.busStationId = end_links.end_busStationId
 ) result
-group by result.start_busLineId
