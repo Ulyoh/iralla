aca8278502bf11c5258d94c230a07e1d971697e9
ulyssey <yohann.m@gmail.com>
2012-01-22 17:46:08 -0600

    start_inter_links_id added to order the intermediate bs
    Change-Id: I64202a032de786609d2cf9cedc968d5e88cc2950

--- iralla_eclipse/mySQL/find_communs_bs.sql
+++ iralla_eclipse/mySQL/find_communs_bs.sql
@@ -5,6 +5,7 @@ select
 	start_max_next_link_id,
 	start_busLineId,
 	end_busStationId as intermediate_bs_id,
+	start_inter_links_id,
 	end_min_previous_link_id,
 	end_max_previous_link_id,
 	end_min_next_link_id,
@@ -17,7 +18,7 @@ select start_bls.start_min_previous_link_id,
 	start_bls.start_max_next_link_id,
 	links.busLineId as start_busLineId,
 	links.busStationId as start_busStationId,
-	links.id as start_id,
+	links.id as start_inter_links_id,
 	end_links.end_min_previous_link_id,
 	end_links.end_max_previous_link_id,
 	end_links.end_min_next_link_id,
@@ -31,7 +32,7 @@ inner join (
 	end_bls.end_max_previous_link_id,
 	end_bls.end_min_next_link_id,
 	end_bls.end_max_next_link_id,
-	links.id as start_id,
+	links.id as end_inter_links_id,
 	links.busLineId as end_busLineId,
 	links.busStationId as end_busStationId
 	from end_bls
@@ -39,3 +40,4 @@ inner join (
 )end_links
 on links.busStationId = end_links.end_busStationId
 ) result
+order by start_busLineId, end_busLineId, start_inter_links_id
