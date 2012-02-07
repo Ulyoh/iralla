24c8e18c786b1d19be90e38fafc74423ee9ec79b
ulyssey <yohann.m@gmail.com>
2012-02-07 12:03:28 -0600

    add $closed param in Polyline
    Change-Id: Id436cc63dd987b99e5fbf3eca16ea5658c917af1

--- iralla_eclipse/preTreatment/tools/php/Polyline.php
+++ iralla_eclipse/preTreatment/tools/php/Polyline.php
@@ -3,13 +3,15 @@
 class Polyline{
 	private $points_array;
 	private $length;
+	private $closed;
 	
 	public function get_points(){
 		return $this->points_array;
 	}
 	
-	public function __construct(array $points_array) {
+	public function __construct(array $points_array, bool $closed = false) {
 		$this->points_array = $points_array;
 		$this->length = count($points_array);
+    	$this->closed = $closed;
 	}
 }
