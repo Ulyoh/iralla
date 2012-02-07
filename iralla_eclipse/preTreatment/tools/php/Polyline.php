3753f84d3ad741c30c4e3b96bd6f19ea9f838a9b
ulyssey <yohann.m@gmail.com>
2012-02-07 12:08:16 -0600

    add Polyline->point_projected($pt)
    Change-Id: Ic1aac6139506d6f93f7cfcbfe2ae543085d44771

--- iralla_eclipse/preTreatment/tools/php/Polyline.php
+++ iralla_eclipse/preTreatment/tools/php/Polyline.php
@@ -9,6 +9,10 @@ class Polyline{
 		return $this->points_array;
 	}
 	
+	public function point_projected(Point $pt){
+		$pt->projection_on_polyline($this);
+	}
+	
 	public function __construct(array $points_array, bool $closed = false) {
 		$this->points_array = $points_array;
 		$this->length = count($points_array);
