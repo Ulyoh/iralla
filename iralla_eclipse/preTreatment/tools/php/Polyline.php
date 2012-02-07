1e81ea7de898534993e7ee37c42a2e9440291fdd
ulyssey <yohann.m@gmail.com>
2012-02-07 13:19:16 -0600

    add Polyline->get_points_between($first_index, $last_index)
    Change-Id: Ifd75990989b404e81adc219cfdde612835aee5bb

--- iralla_eclipse/preTreatment/tools/php/Polyline.php
+++ iralla_eclipse/preTreatment/tools/php/Polyline.php
@@ -9,6 +9,24 @@ class Polyline{
 		return $this->points_array;
 	}
 	
+	/**
+	 * 
+	 * @param int $first_index
+	 * @param int $last_index
+	 * @return array / false
+	 */
+	public function get_points_between(int $first_index,int $last_index){
+		if($first_index <= $last_index){
+			return array_slice($this->points_array, $first_index, $last_index-$first_index);
+		}
+		else if ($this->closed === true){
+			return array_merge(array_slice($this->points_array, $last_index), array_slice($this->points_array, 0, $first_index + 1));
+		}
+		else{
+			return false;
+		}
+	}
+	
 	public function point_projected(Point $pt){
 		$pt->projection_on_polyline($this);
 	}
