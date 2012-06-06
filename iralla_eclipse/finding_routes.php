84c46ce6025a20a4b4b5d5c2547a151dc4217082
ulyssey <yohann.m@gmail.com>
2012-06-06 16:41:20 -0500

    rewrite algo to find route form finding_routes.php
    Change-Id: If016011c046cb5c3951bb6f99c0fe558e84e1e3a

--- iralla_eclipse/finding_routes.php
+++ iralla_eclipse/finding_routes.php
@@ -2,8 +2,8 @@
 require_once 'access_to_db.php';
 require_once 'tools.php';
 require_once 'Busline.php';
-require_once 'finding_routes/find_routes.php';
-
+require_once 'finding_routes/find_first_and_last_squares_of_nearest_bls.php';
+require_once 'functions_for_finding_route_3.php';
 
 ini_set('memory_limit', "2000M");
 $multipicador = 10000000;
@@ -17,13 +17,17 @@ $bus_speed = 7;
 $request = '	{"start":{"lat":-2.089031793406955,"lng":-79.90463485717771},"end":{"lat":-2.2722333014760494,"lng":-79.88300552368162}}';
 $request = json_decode($request);
 
-//extract start and end point
-$start['lat'] = $request->start->lat;
-$start['lng'] = $request->start->lng;
-$end['lat'] = $request->end->lat;
-$end['lng'] = $request->end->lng;
+//extract start and end point;
+$start = new Point($request->start->lng, $request->start->lat);
+$end = new Point($request->end->lng, $request->end->lat);
 
-//find the routes
-$results = find_routes($start, $end);
+//find nearest bls of start point
+//create $start->first_and_last_squares
+$start_squares = find_first_and_last_square_of_nearest_bls($start);
+
+//idem for end point:
+$end_squares = find_first_and_last_square_of_nearest_bls($end);
+
+
 
 echo json_encode($results);
