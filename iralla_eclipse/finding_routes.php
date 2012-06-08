e2ddfc8abf9e3121cb04a9bd80fb3cc07f0729da
ulyssey <yohann.m@gmail.com>
2012-06-07 20:07:28 -0500

    to pass arguments to communs_lines_in_start_and_end_squares()
    Change-Id: I6f42118c687b2f5c4184aa60b677c55d7d05e47f

--- iralla_eclipse/finding_routes.php
+++ iralla_eclipse/finding_routes.php
@@ -3,6 +3,7 @@ require_once 'access_to_db.php';
 require_once 'tools.php';
 require_once 'Busline.php';
 require_once 'finding_routes/find_first_and_last_squares_of_nearest_bls.php';
+require_once 'finding_routes/communs_lines_in_start_and_end_squares.php';
 require_once 'functions_for_finding_route_3.php';
 
 ini_set('memory_limit', "2000M");
@@ -23,11 +24,33 @@ $end = new Point($request->end->lng, $request->end->lat);
 
 //find nearest bls of start point
 //create $start->first_and_last_squares
-$start_squares = find_first_and_last_square_of_nearest_bls($start);
+find_first_and_last_square_of_nearest_bls($start);
 
 //idem for end point:
-$end_squares = find_first_and_last_square_of_nearest_bls($end);
+find_first_and_last_square_of_nearest_bls($end);
+
+//look for communs buslines in $start_squares and $end_squares
+$results = communs_lines_in_start_and_end_squares($start->first_and_last_squares, $end->first_and_last_squares);
+	
+if ($results != false){
+	echo json_encode($results);
+	exit();
+}
+
+//look if two lines from start and end cross each other:
+$results = $cross_lines_in_start_and_end_squares($start_squares, $end_squares);
+if ($results != false){
+	echo json_encode($results);
+	exit();
+}
+
+//look if a third line exists to link two lines from start to end:
+$results = $third_lines_from_start_bl_to_end_squares_bl($start_squares, $end_squares);
+if ($results != false){
+	echo json_encode($results);
+	exit();
+}
 
 
 
-echo json_encode($results);
+
