d71a34b1eda28f1160c2689704309d9c2c625cc8
ulyssey <yohann.m@gmail.com>
2012-06-09 14:54:57 -0500

    communs_lines_in_start_and_end_squares.php : structure finished
    Change-Id: Id276f844298235d1f89b5bc4af8f0a86b2a5cfce

--- iralla_eclipse/finding_routes/communs_lines_in_start_and_end_squares.php
+++ iralla_eclipse/finding_routes/communs_lines_in_start_and_end_squares.php
@@ -1,29 +1,54 @@
 <?php
 function communs_lines_in_start_and_end_squares(Point $start,Point $end){
+	global $bdd;
+	
 	$start_f_and_l_squares = $start->first_and_last_squares;
 	$end_f_and_l_squares = $end->first_and_last_squares;
 	
-	$current_start_f_and_l_square = $start_f_and_l_squares[0];
-	$current_end_f_and_l_square = $end_f_and_l_squares[0];
-	$i_start = 0;
-	$i_end = 0;
-	$first_i_end_to_match_with_start_f_and_l_square = null;
+	//extract bl_ids list:
+	$bl_ids_list = $start_f_and_l_squares[0]['first']['bl_id'];
+	$i = 1;
+	$j = 0;
+	while ($start_bl_id = $start_f_and_l_squares[$i]['first']['bl_id']){
+		while ($start_f_and_l_squares[$j]['first']['bl_id'] < $start_f_and_l_squares[$i]['first']['bl_id']){
+			$j++;
+		}
+		if($start_f_and_l_squares[$j]['first']['bl_id'] == $start_f_and_l_squares[$i]['first']['bl_id']){
+			$bl_ids_list = ' OR id = ' . $start_f_and_l_squares[$j]['first']['bl_id'];
+		}
+		$i++;
+	}
 	
-	
-	//extract all bl with bl_ids in start and end square
-	
-	
-	
+	//extract bus lines:
+	$query =  file_get_contents('extract_bus_lines_depend_on_where_search.sql');
+	$req = $bdd->query($query);
 	
 	//for each bl exctracted
-	$bls_ids_to_calculate = array();
 	$bl_id_to_calculate = 0;
-	$i = $j = $k = 0;
-	while($bl_id_to_calculate = $bls_ids_to_calculate[$i++]){
+	$j = $k = 0;
+	
+	while($bus_line_datas = $req->fetch()){
+	$bl_id_to_calculate = $bus_line_datas['id'];
+
 		A:while($start_f_and_l_squares[$j++]['first']['bl_id'] < $bl_id_to_calculate);
 		$first_k_for_this_bl_id = $k;
 		B:while($end_f_and_l_squares[$k++]['first']['bl_id'] < $bl_id_to_calculate);
-	
+			
+			//create bus line object:
+			$bl = new Busline($bus_line_datas['path'], $bus_line_datas['name']);
+			$bl->flow = $bus_line_datas['flow'];
+			
+			//calculate nearest point on the bus line from the start point
+			$start->projection_on_polyline_between_two_squares($bl, $start_f_and_l_squares['first'], $start_f_and_l_squares['last']);
+			
+			//calculate nearest point on the bus line from the end point
+			$end->projection_on_polyline_between_two_squares($bl, $start_f_and_l_squares['first'], $start_f_and_l_squares['last']);
+			
+			//calculate the road
+			$results[] = caculate_road_on_one_bus_line(
+					$bl,
+					$start,
+					$end);
 	
 		if($end_f_and_l_squares['first']['bl_id'] == $bl_id_to_calculate){
 			goto B;
@@ -35,50 +60,7 @@ function communs_lines_in_start_and_end_squares(Point $start,Point $end){
 	}
 	
 	
-	while($current_start_f_and_l_square != null) {
-		//bl_ids was ordered in the mySQL request
-		while($current_end_f_and_l_square &&
-		 ($current_end_f_and_l_square['first']['bl_id'] < $current_start_f_and_l_square['first']['bl_id']) ){
-			
-			$i_end++;
-			$current_end_f_and_l_square = $end_f_and_l_squares[$i_end];
-		}
-		
-		//in case start bl_id is the same that the previous start bl_id
-		//and at least one square had the same bl_id
-		if(($first_i_end_to_match_with_start_f_and_l_square != null)
-				
-			&& ($end_f_and_l_squares[$first_i_end_to_match_with_start_f_and_l_square]['first']['bl_id'] 
-				== $current_start_f_and_l_square['first']['bl_id'])){
-			
-			//reinit $i_end to the first end_f_and_l_square with the same bl_id
-			$i_end = $first_i_end_to_match_with_start_f_and_l_square;
-		}
-		
-		
-		$first_i_end_to_match_with_start_f_and_l_square = $i_end;
-		
-		while(($current_end_f_and_l_square != null) &&
-		 ($current_end_f_and_l_square['first']['bl_id'] == $current_start_f_and_l_square['first']['bl_id']) ){
-			
-			$elts_to_calculate_road = array();
-			$elts_to_calculate_road['start_square'] = $current_start_f_and_l_square;
-			$elts_to_calculate_road['end_square'] = $current_end_f_and_l_square;
-			
-			$i_end++;
-			$current_end_f_and_l_square = $end_f_and_l_squares[$i_end];
-		}
-		
-		$i_start++;
-		$current_start_f_and_l_square = $start_f_and_l_squares[$i_start];
-	}
-	
-	//extract the bus lines needes from the data base
-	$start->projection_on_polyline_between($p, $first_index, $last_index);
-	
-	//calculate the nearest point
-	
-	//extract the road
+	return $results;
 	
 }
 
