0f2588e795c9a8e0b8a94a56d51e5b356a66e52c
ulyssey <yohann.m@gmail.com>
2012-06-09 13:25:16 -0500

    improve loops
    Change-Id: Idb5ba73bbd174827718d84b8563c671799f767e0

--- iralla_eclipse/finding_routes/communs_lines_in_start_and_end_squares.php
+++ iralla_eclipse/finding_routes/communs_lines_in_start_and_end_squares.php
@@ -7,8 +7,9 @@ function communs_lines_in_start_and_end_squares($start, $end){
 	$current_end_f_and_l_square = $end_f_and_l_squares[0];
 	$i_start = 0;
 	$i_end = 0;
+	$first_i_end_to_match_with_start_f_and_l_square = null;
 	
-	while($current_start_f_and_l_square) {
+	while($current_start_f_and_l_square != null) {
 		//bl_ids was ordered in the mySQL request
 		while($current_end_f_and_l_square &&
 		 ($current_end_f_and_l_square['first']['bl_id'] < $current_start_f_and_l_square['first']['bl_id']) ){
@@ -16,7 +17,22 @@ function communs_lines_in_start_and_end_squares($start, $end){
 			$i_end++;
 			$current_end_f_and_l_square = $end_f_and_l_squares[$i_end];
 		}
-		while($current_end_f_and_l_square &&
+		
+		//in case start bl_id is the same that the previous start bl_id
+		//and at least one square had the same bl_id
+		if(($first_i_end_to_match_with_start_f_and_l_square != null)
+				
+			&& ($end_f_and_l_squares[$first_i_end_to_match_with_start_f_and_l_square]['first']['bl_id'] 
+				== $current_start_f_and_l_square['first']['bl_id'])){
+			
+			//reinit $i_end to the first end_f_and_l_square with the same bl_id
+			$i_end = $first_i_end_to_match_with_start_f_and_l_square;
+		}
+		
+		
+		$first_i_end_to_match_with_start_f_and_l_square = $i_end;
+		
+		while(($current_end_f_and_l_square != null) &&
 		 ($current_end_f_and_l_square['first']['bl_id'] == $current_start_f_and_l_square['first']['bl_id']) ){
 			
 			//calcul the nearest point from start point
