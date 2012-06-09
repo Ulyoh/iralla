9d3fb8388253a8674b22a2bd7afe36a037be3bdd
ulyssey <yohann.m@gmail.com>
2012-06-09 13:09:06 -0500

    modify to handle $first_and_last_squares[]
    Change-Id: I5390bef84887c54947004845ed5e194b8b6053c1

--- iralla_eclipse/finding_routes/communs_lines_in_start_and_end_squares.php
+++ iralla_eclipse/finding_routes/communs_lines_in_start_and_end_squares.php
@@ -1,29 +1,40 @@
 <?php
-function communs_lines_in_start_and_end_squares($start_squares, $end_squares){
-	$current_start_square = $start_squares[0];
-	$current_end_square = $end_squares[0];
-	$i = 0;
-	/**
-	 * TODO ameliorer la boucle ci dessous
-	 * (the bl_ids are in order from the mySQL search)
-	 */
-	while($current_start_square) {
-		while($current_end_square && ($current_end_square['bl_id'] < $current_start_square['bl_id']) ){
-			$i += 2;
-			$current_square = $end_squares[$i];
+function communs_lines_in_start_and_end_squares($start, $end){
+	$start_f_and_l_squares = $start->first_and_last_squares;
+	$end_f_and_l_squares = $end->first_and_last_squares;
+	
+	$current_start_f_and_l_square = $start_f_and_l_squares[0];
+	$current_end_f_and_l_square = $end_f_and_l_squares[0];
+	$i_start = 0;
+	$i_end = 0;
+	
+	while($current_start_f_and_l_square) {
+		//bl_ids was ordered in the mySQL request
+		while($current_end_f_and_l_square &&
+		 ($current_end_f_and_l_square['first']['bl_id'] < $current_start_f_and_l_square['first']['bl_id']) ){
+			
+			$i_end++;
+			$current_end_f_and_l_square = $end_f_and_l_squares[$i_end];
 		}
-		if($current_end_square['bl_id'] == $current_start_square['bl_id']){
+		while($current_end_f_and_l_square &&
+		 ($current_end_f_and_l_square['first']['bl_id'] == $current_start_f_and_l_square['first']['bl_id']) ){
+			
 			//calcul the nearest point from start point
 			//add infos needed to calcul the road to $start_pt_on_bl
+			$first_square = $start_f_and_l_squares[$i_start];
+			$last_square = $start_f_and_l_squares[$i_start + 1];
 			
 			//calcul the nearest point from end point
 			//add infos needed to calcul the road to $end_pt_on_bl
 			
 			
 			$results[] = bus_line_from_to($start_pt_on_bl, $end_pt_on_bl);
+			
+			$i_end++;
+			$current_end_f_and_l_square = $end_f_and_l_squares[$i_end];
 		}
 		
-		$i += 2;
-		$current_start_square = $start_squares[$i];
+		$i_start++;
+		$current_start_f_and_l_square = $start_f_and_l_squares[$i_start];
 	}
 }
