3f53cf9f9e0f691c7be78104a2d4456368e991e2
ulyssey <yohann.m@gmail.com>
2012-06-11 20:00:48 -0500

    add columns in select to calculate the distance to the proyection pt
    Change-Id: I7794af4dd6c52b7e8b1b70cd7ac463906db733af

--- iralla_eclipse/finding_routes/select_squares_near_a_pt.sql
+++ iralla_eclipse/finding_routes/select_squares_near_a_pt.sql
@@ -1,5 +1,19 @@
-select id, bl_id, lat, lng, prev_index_of_next_link, prev_index_of_prev_link, flows
+select 
+	id, 
+	bl_id, 
+	lat, 
+	lng, 
+	prev_index_of_next_link, 
+	prev_index_of_prev_link, 
+	flows, 
+	prev_index_of_pt,
+	distance_from_first_vertex,
+	pt_coords_lat,
+	pt_coords_lng
 from squares
 where lat between ? and ?
 and lng between ? and ?
 order by bl_id, id
+
+
+		
