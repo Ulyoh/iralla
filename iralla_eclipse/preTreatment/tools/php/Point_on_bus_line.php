daabad1fd9041e874039f9b605b18d9818c40e47
ulyssey <yohann.m@gmail.com>
2012-06-12 21:47:29 -0500

    move the Point->distance_from_first_vertex_with_square_infos() to Point_on_bus_line
    Change-Id: I60fdc2f527744b806bae6ff58de255a36f70410d

--- iralla_eclipse/preTreatment/tools/php/Point_on_bus_line.php
+++ iralla_eclipse/preTreatment/tools/php/Point_on_bus_line.php
@@ -5,6 +5,31 @@ class Point_on_bus_line extends Point{
 	public $distance_from_previous_index;
 	public $bus_station;
 	
+	public function distance_from_first_vertex_with_square_infos(Busline $bl, $f_and_l_square){
+		//TODO revoir la base de donnee pour simplifer se calcul
+		//ie connaitre la distance de chaque vertex a partir du debut
+		$square_pt = new Point(
+				$f_and_l_square['first']['pt_coords_lng'],
+				$f_and_l_square['first']['pt_coords_lat']);
+		$previous_index_of_square = (int) $f_and_l_square['first']['prev_index_of_pt'];
+		$previous_vertex_pt = $bl->get_point_at($previous_index_of_square);
+	
+		$distance_from_first_to_previous_vertex_of_square =
+		$f_and_l_square['first']['distance_from_first_vertex']
+		-
+		$square_pt->earth_distance_to($previous_vertex_pt);
+	
+		$previous_index_of_this = $this->projection_infos['index'];
+		$previous_vertex_of_this = $bl->get_point_at($previous_index_of_this);
+	
+		$this->distance_from_first_vertex =
+		$distance_from_first_to_previous_vertex_of_square
+		+
+		$bl->calculate_distance_between_2_vertex($previous_index_of_square, $previous_index_of_this)
+		+
+		$previous_vertex_of_this->earth_distance_to($this);
+	}
+	
 	public function __construct(
 			$x,
 			$y,
