3fbe5c902186638abd0f970085d88a371640eb1b
ulyssey <yohann.m@gmail.com>
2012-06-12 23:09:01 -0500

    Bug in busline->get_points_from_start_to_end_pts_on_bl
    Change-Id: I25ab8b3bd06c37561bd9aa92a662dc49f491659d

--- iralla_eclipse/preTreatment/tools/php/Busline.php
+++ iralla_eclipse/preTreatment/tools/php/Busline.php
@@ -133,7 +133,7 @@ class Busline extends Polyline{
 	}
 	
 	public function get_points_from_start_to_end_pts_on_bl(Point $start_point, Point $end_point){
-		$resutls = $this->get_points_between_start_and_end_pts_on_bl($start_point, $end_point);
+		$results = $this->get_points_between_start_and_end_pts_on_bl($start_point, $end_point);
 		foreach ($results as $result) {
 			array_unshift($result, $start_point);
 			$result[] = $end_point;
